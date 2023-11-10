5a16344b22872f493542b383c6d482c56389e386
fadhlaouir <raed.fadhlaoui@hotmail.com>
2023-11-10 12:26:11 +0100

    feat: Implement Domain Core

---
+++ HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Domain.Common
+{
+    public abstract class BaseEntity // absract class cannot be instantiated but can be inherited from other classes
+    {
+        public int Id { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public DateTime? DateModified { get; set; }
+    }
+}
